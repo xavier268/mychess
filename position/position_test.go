04ace3cc25c6fd948293c6dd1b92f17a1daa0fc3
xavier@DellXPSA <xavier@DellXPSA>
2025-03-03 20:17:34 +0100

    more tests & bug fixes

--- position/position_test.go
+++ position/position_test.go
@@ -21,29 +21,50 @@ func TestRandomPosition(t *testing.T) {
 		AddBishop(WHITE, "a2", "a3").
 		AddQueen(BLACK, "e4").
 		AddRook(BLACK, "h8").
-		AddPawn(BLACK, "a7", "b6").AddPawn(WHITE, "d2", "e3")
+		AddPawn(BLACK, "a7", "b6").AddPawn(WHITE, "d2", "e3", "h5")
 	p.UpdateKingThreats(bt)
 	fmt.Println(p)
 	fmt.Println(p.status)
 
-	/*
-		sq := SqParse("c2")
-		fmt.Println("Is C2 attacked by WHITE :", p.IsSquareAttacked(bt, sq, WHITE))
-		fmt.Println("Is C2 attacked by BLACK :", p.IsSquareAttacked(bt, sq, BLACK))
-		fmt.Println("C2 Bishop BLACK attack set ", p.GetBishopMovesFromSquare(bt, BLACK, sq).String())
-		fmt.Println("C2 Bishop WHITE attack set ", p.GetBishopMovesFromSquare(bt, WHITE, sq).String())
+	// Verify attacks ...
 
-		sq = SqParse("f4")
-		fmt.Println("Is F4 attacked by WHITE :", p.IsSquareAttacked(bt, sq, WHITE))
-		fmt.Println("WHITE Pawns move from F4", p.GetPawnMovesFromSquare(bt, WHITE, sq).String())
-		fmt.Println("BLACK Pawns move from F4", p.GetPawnMovesFromSquare(bt, BLACK, sq).String())
-	*/
+	type wb struct {
+		w bool // attacked by white ?
+		b bool // attacked by black ?
+	}
+	testSqu := map[string]wb{
+		// no attacks
+		"c7": {false, false},
+		"g7": {false, false},
+		"h3": {false, false},
+		"a6": {false, false},
 
-	for _, s := range []string{"c7", "f4", "h5"} {
+		// both attacks
+		"f4": {true, true},
+		"d4": {true, true},
+		"e3": {true, true},
+		"d6": {true, true},
+
+		// black only attacks
+		"h5": {false, true},
+		"a5": {false, true},
+		"b7": {false, true},
+
+		// white only attacks
+		"d1": {true, false},
+	}
+
+	for s, res := range testSqu {
 		sq := SqParse(s)
 		fmt.Println(p)
 		fmt.Println("Square", sq.String(), "is attacked by WHITE :", p.IsSquareAttacked(bt, sq, WHITE))
 		fmt.Println("Square", sq.String(), "is attacked by BLACK :", p.IsSquareAttacked(bt, sq, BLACK))
+		if p.IsSquareAttacked(bt, sq, WHITE) != res.w {
+			t.Error("Square", sq.String(), "is attacked by WHITE :", p.IsSquareAttacked(bt, sq, WHITE))
+		}
+		if p.IsSquareAttacked(bt, sq, BLACK) != res.b {
+			t.Error("Square", sq.String(), "is attacked by BLACK :", p.IsSquareAttacked(bt, sq, BLACK))
+		}
 	}
 
 }
